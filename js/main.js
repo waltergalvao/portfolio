a842bad555f13bae040dbb7cefecdffb271051c7
Walter Galvão <walter.galvao@biper.com.br>
2019-07-24 18:56:01 -0700

    UX & Text adjustments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 /*===Navigation Drawer===*/
 function openNavigationDrawer() {
     navigationDrawer.classList.add("navigation__drawer--open");
-    navigationMask.classList.remove("d-none");
+    navigationMask.classList.add("navigation__mask--visible");
+    document.body.style.overflow = "hidden";
 }
 
 function closeNavigationDrawer() {
     navigationDrawer.classList.remove("navigation__drawer--open");
-    navigationMask.classList.add("d-none");
+    navigationMask.classList.remove("navigation__mask--visible");
+    document.body.style.overflow = "auto";
 }
 
 openNavigation.onclick = () => openNavigationDrawer();
