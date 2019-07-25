ed56596396f5daecf5c7f7819519a812ce1eb36c
Walter Galvão <walter.galvao@biper.com.br>
2019-07-24 23:01:47 -0700

    Changed fetch to XMLHttpRequest due compatibility issues

--- js/blog.js
+++ js/blog.js
@@ -1,9 +1,10 @@
+// Parse and display feed
 function displayPosts(data) {
     let postHtml = '';
 
     for (let i=0; i < data.items.length; i++) {
         let post = data.items[i];
-        console.log(post);
+
         postHtml += `
             <div class="post">
                 <a href="${post.link}" target="_blank" rel="nofollow" class="post__content">
@@ -22,17 +23,15 @@ function displayPosts(data) {
     mediumPosts.innerHTML = postHtml;
 }
 
-fetch('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40wbgneto')
-    .then(
-        function (response) {
-            if (response.status !== 200) {
-                alert("Could not load posts, please try again later");
-                return;
-            }
+// Requests medium feed
+let request = new XMLHttpRequest();
+request.open('GET', 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40wbgneto');
+request.send();
 
-            response.json().then(displayPosts);
-        }
-    )
-    .catch(function (err) {
-        alert("Could not load posts, please try again later");
-    });
+request.onload = function () {
+    if (this.status === 200) {
+        return displayPosts(JSON.parse(this.responseText));
+    }
+
+    alert("Could not load posts, please try again later");
+};
